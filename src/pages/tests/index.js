2ee5086849bb02636932858d9f4e7ddf17e9aba3
Kiko Climent <kikocliment@MacBook-Air-3.fritz.box>
2026-01-07 19:49:38 +0100

    Explorations with threejs and distortion slider with bg noise2

---
+++ src/pages/tests/index.js
@@ -0,0 +1,14 @@
+import HeroCarousel7 from "@/components/threeslider/index8";
+import HeaderFooter8 from "@/components/menu/index8";
+
+export default function Tests() {
+
+
+  return (
+    <div className="h-full w-full overflow-hidden">
+      <HeaderFooter8>
+        <HeroCarousel7 />
+      </HeaderFooter8>
+    </div>
+  );
+}
