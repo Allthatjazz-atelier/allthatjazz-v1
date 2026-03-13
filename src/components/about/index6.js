ce05aac1e11f0859e6cce51ceffdfaca430975ec
Kiko Climent <kikocliment@Mac.fritz.box>
2026-03-13 15:03:54 +0100

    Fixing about padding on mobiles

--- src/components/about/index6.js
+++ src/components/about/index6.js
@@ -100,20 +100,20 @@ export default function AboutSection6() {
   };
 
   return (
-    <div className="relative w-full h-screen flex flex-col text-[0.875rem] md:text-[0.875rem] tracking-[-0.04em] items-center justify-center px-4 leading-none text-center">
+    <div className="relative w-full h-screen gap-12 flex flex-col text-[0.875rem] md:text-[0.875rem] tracking-[-0.04em] items-center justify-start pt-[16vh] md:pt-0 md:justify-center px-4 leading-none text-center">
       
       {/* TEXTOS ORIGINALES */}
       <div className="flex flex-col z-10 w-[100vw] md:w-[40vw] px-4">
-        <p className="flex font-semibold text-[1.35rem] md:text-[1.25rem] tracking-[-0.045em] leading-[1.1] mb-[2rem]">
+        <p className="flex font-semibold text-[1.25rem] tracking-[-0.045em] leading-[1.1] mb-[2rem]">
           A creative collective based in Berlin, playing Worldwide. <br/> Blending digital craft with analog attitude. Exploring and shaping visual narratives and experiences that resonate through culture, music, fashion, lifestyle and multimedia. Hustling 24/7.
         </p>
-        <p className="flex font-semibold text-[1.35rem] md:text-[1.25rem] tracking-[-0.045em] leading-[1.1] mb-[4rem]">
+        <p className="flex font-semibold text-[1.25rem] tracking-[-0.045em] leading-[1.1] mb-[4rem]">
           Creative direction, strategy, digital and web experiences, design and code are part of our playground
         </p>
         <div className="flex flex-col">
-          <p className="font-semibold text-[1.35rem] md:text-[1.25rem] tracking-[-0.045em] leading-[1.1]">For collaborations or inquires, please reach out at:</p>
-          <p className="font-semibold text-[1.35rem] md:text-[1.25rem] tracking-[-0.045em] leading-[1.1]">hello@allthatjazz.com</p>
-          <p className="uppercase font-semibold text-[0.75rem] md:text-[0.625rem] tracking-[-0.045em] leading-[1.1] mt-1">joaquin diaz, kiko Climent, +++</p>
+          <p className="font-semibold text-[1.25rem] tracking-[-0.045em] leading-[1.1]">For collaborations or inquires, please reach out at:</p>
+          <p className="font-semibold text-[1.25rem] tracking-[-0.045em] leading-[1.1]">hello@allthatjazz.com</p>
+          <p className="uppercase font-semibold text-[0.625rem] tracking-[-0.045em] leading-[1.1] mt-1">joaquin diaz, kiko Climent, +++</p>
         </div>
       </div>
       
